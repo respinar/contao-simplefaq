372732a5d300e17adb8cf04eb6010b8e6006ddf0
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-09-08 16:55:45 +0330

    remove schema section

--- src/Controller/ContentElement/SimplefaqItemController.php
+++ src/Controller/ContentElement/SimplefaqItemController.php
@@ -18,21 +18,7 @@ class SimplefaqItemController extends AbstractContentElementController
     public const TYPE = 'simplefaq_item';
 
     protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response
-    {
-
-        // Store the FAQ item in a session or temporary storage
-        if (!isset($GLOBALS['FAQ_ITEMS'])) {
-            $GLOBALS['FAQ_ITEMS'] = [];
-        }
-
-        $GLOBALS['FAQ_ITEMS'][] = [
-            "@type" => "Question",
-            "name" => strip_tags($model->simplefaq_question),
-            "acceptedAnswer" => [
-                "@type" => "Answer",
-                "text" => strip_tags($model->simplefaq_answer)
-            ]
-        ];
+    {        
 
         $template->set('question', $model->simplefaq_question);
         $template->set('answer', $model->simplefaq_answer);
