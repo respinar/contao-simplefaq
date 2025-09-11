8d2b21879f977e8f88f33cb030b20981d424e6a1
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-09-11 22:38:20 +0330

    ecs suggestion

--- src/Controller/ContentElement/SimplefaqItemController.php
+++ src/Controller/ContentElement/SimplefaqItemController.php
@@ -14,15 +14,13 @@ use Symfony\Component\HttpFoundation\Response;
 #[AsContentElement(category: 'simplefaq')]
 class SimplefaqItemController extends AbstractContentElementController
 {
-
     public const TYPE = 'simplefaq_item';
 
     protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response
-    {        
-
+    {
         $template->set('question', $model->simplefaq_question);
         $template->set('answer', $model->simplefaq_answer);
-        
+
         return $template->getResponse();
     }
 }
