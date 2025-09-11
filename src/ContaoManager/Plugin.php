85f572e88b651dce7c111c4a8f8ad75fe4ca8225
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-09-11 22:37:58 +0330

    ecs suggestion

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -16,9 +16,6 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Symfony\Component\Config\Loader\LoaderResolverInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
-use Symfony\Component\Routing\RouteCollection;
 use Respinar\ContaoSimplefaqBundle\ContaoSimplefaqBundle;
 
 class Plugin implements BundlePluginInterface
